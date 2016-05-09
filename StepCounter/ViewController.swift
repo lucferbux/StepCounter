d62139cb1685a037d75bc20ff64094e59af277f3
lucas fernández <6lucyfer9@gmail.com>
2016-05-09 19:05:19 +0200

    Initial Commit

---
+++ StepCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StepCounter
+//
+//  Created by lucas fernández on 09/05/16.
+//  Copyright © 2016 lucas fernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ StepCounterTests/StepCounterTests.swift
@@ -0,0 +1,36 @@
+//
+//  StepCounterTests.swift
+//  StepCounterTests
+//
+//  Created by lucas fernández on 09/05/16.
+//  Copyright © 2016 lucas fernández. All rights reserved.
+//
+
+import XCTest
+@testable import StepCounter
+
+class StepCounterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
